c750c87b98ed7033d96a34cc755a6382356370f0
N. Inta <5730242+MoNoLidThZ@users.noreply.github.com>
2025-08-11 17:16:44 -0700

    Add Givepet Feature

---
+++ src/main/java/project/art3mis/plugin/GivePetInteractListener.java
@@ -0,0 +1,55 @@
+package project.Art3mis.Plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+public class GivePetInteractListener implements Listener {
+
+    private final Main plugin;
+
+    public GivePetInteractListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void on(PlayerInteractEntityEvent e) {
+        if (e.getHand() != EquipmentSlot.HAND) return;
+
+        if (!(e.getRightClicked() instanceof Tameable target)) {
+            return;
+        }
+        final var uuid = e.getPlayer().getUniqueId();
+
+        GivePetTransferAttempt foundAttempt = plugin.transferAttempts().get(uuid);
+        if (foundAttempt == null) return;
+
+        e.setCancelled(true);
+
+        AnimalTamer owner = target.getOwner();
+        if (owner != null && owner.getUniqueId().equals(uuid)) {
+            Player newOwner = Bukkit.getPlayer(foundAttempt.receiver());
+            if (newOwner == null) {
+                e.getPlayer().sendMessage(plugin.getMsg("playerLeft"));
+                plugin.transferAttempts().remove(foundAttempt);
+                return;
+            }
+            target.setOwner(newOwner);
+            target.teleport(newOwner);
+            newOwner.sendMessage(plugin.getMsg("sentReceiverMsg")
+                    .replace("{sender}", e.getPlayer().getDisplayName())
+                    .replace("{type}", target.getType().toString().toLowerCase())
+            );
+            e.getPlayer().sendMessage(plugin.getMsg("sentSenderMsg"));
+        } else {
+            e.getPlayer().sendMessage(plugin.getMsg("notOwned"));
+        }
+
+        plugin.transferAttempts().remove(uuid);
+    }
+}
--- src/main/java/project/art3mis/plugin/Main.java
+++ src/main/java/project/art3mis/plugin/Main.java
@@ -8,7 +8,67 @@ import org.bukkit.event.Listener;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends JavaPlugin implements Listener{
 
+    public static final Pattern patternBrackets = Pattern.compile("\\{#[0-9a-fA-F]{6}\\}");
+    public static final Pattern pattern = Pattern.compile("#[0-9a-fA-F]{6}");
+
+    private final Map<UUID, GivePetTransferAttempt> transferAttempts = new HashMap<>();
+
+    public Map<UUID, GivePetTransferAttempt> transferAttempts() {
+        return transferAttempts;
+    }
+
+    public boolean cancelTransfer(UUID uuid) {
+        return transferAttempts.remove(uuid) != null;
+    }
+
+    public static String colorMsg(String s) {
+        s = removeBrackets(s);
+        Matcher match = pattern.matcher(s);
+        while (match.find()) {
+            String color = s.substring(match.start(), match.end());
+            s = s.replace(color, "" + net.md_5.bungee.api.ChatColor.of(color));
+            match = pattern.matcher(s);
+        }
+        return net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    private static String removeBrackets(String text) {
+        Matcher m = patternBrackets.matcher(text);
+        String replaced = text;
+        while (m.find()) {
+            String hexcode = m.group();
+            String fixed = hexcode.substring(2, 8);
+            replaced = replaced.replace(hexcode, "#" + fixed);
+        }
+        return replaced;
+    }
+
+    public String getMsg(String key) {
+        return colorMsg(getConfig().getString(key));
+    }
+
+    @Override
     public void onEnable() {
+        FileConfiguration cfg = getConfig();
+        cfg.options().copyDefaults(true);
+        cfg.addDefault("rightClickPrompt", "&ePlease right click the pet you would like to give");
+        cfg.addDefault("playerNotFound", "&cThat player could not be found!");
+        cfg.addDefault("cancelFail", "&cYou haven't tried to transfer a pet!");
+        cfg.addDefault("cancelSuccess", "&aCancelled transferring a pet!");
+        cfg.addDefault("playerLeft", "&cThe player you were trying to give that pet to has since left the server.");
+        cfg.addDefault("sentReceiverMsg", "{sender}&a gave you a pet &f{type}&a!");
+        cfg.addDefault("sentSenderMsg", "&aYou gave your pet successfully!");
+        cfg.addDefault("notOwned", "&cThat's not your pet!");
+        cfg.addDefault("selfGive", "&cYou can't give a pet to yourself!");
+        saveConfig();
+
+        GivePetCommand cmd = new GivePetCommand(this);
+
+        getCommand("givepet").setExecutor(cmd);
+        getCommand("givepet").setTabCompleter(cmd);
+
+        getServer().getPluginManager().registerEvents(new GivePetInteractListener(this), this);
+
         getCommand("map").setExecutor(new MapCommand());
     }
     public static void main(String[] args) {
